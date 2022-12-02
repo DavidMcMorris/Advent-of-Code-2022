4a08f22b3c69a87439df86a99cd0b1ae1d03e95c
DavidMcMorris <david.mcmorris@huskers.unl.edu>
2022-12-02 14:16:37 -0500

    Minor formatting changes

--- Day 1/Day1.R
+++ Day 1/Day1.R
@@ -4,14 +4,14 @@ input <- readLines("Day_1_input.txt")
 input <- as.numeric(input)
 
 
-inds <- c(1,which(is.na(input)))
-input[which(is.na(input))]<-0
+inds <- c(1, which(is.na(input)))
+input[which(is.na(input))] <- 0
 
 calories <- NULL
 
-for(i in 1:(length(inds) - 2)){
-	calories[i] <- sum(input[inds[i]:inds[i+1]])
+for (i in 1:(length(inds) - 2)){
+    calories[i] <- sum(input[inds[i]:inds[i + 1]])
 }
 
-MaxCal <- max(calories)
-MaxThree <- sum(rev(sort(calories))[1:3])
+max_cal <- max(calories)
+max_three <- sum(rev(sort(calories))[1:3])
