83eea93b06db5ce07d6e43436eb39b7814ee4081
DavidMcMorris <david.mcmorris@huskers.unl.edu>
2022-12-04 18:04:45 -0500

    Tried part 2, not working

--- Day 4/Day4.R
+++ Day 4/Day4.R
@@ -11,6 +11,24 @@ containment <- function(assignments) {
     }
 }
 
+overlap <- function(assignments) {
+    if (assignments[1] <= assignments[3] && assignments[2] >= assignments[3] || assignments[1] <= assignments[4] && assignments[2] >= assignments[4]) {
+        return(1)
+    } else {
+        return(0)
+    }
+}
 
-p1_total <- sum(apply(input, 1,containment))
+edge <- function(assignments) {
+    if (assignments[1] == assignments[3] && assignments[2] == assignments[4] || assignments[1] == assignments[3] && assignments[2] == assignments[4]) {
+        return(1)
+    } else {
+    return(0)
+}
+
+}
+p1_total <- sum(apply(input, 1, containment))
+p2_total <- sum(apply(input, 1, overlap)) + p1_total - sum(apply(input, 1, edge))
+
 print(p1_total)
+print(p2_total)
