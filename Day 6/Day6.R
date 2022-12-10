fb1b2ef9d824206e13550afa07f90dade551e897
DavidMcMorris <david.mcmorris@huskers.unl.edu>
2022-12-10 14:05:37 -0500

    Adaptable for p1/p2

--- Day 6/Day6.R
+++ Day 6/Day6.R
@@ -2,15 +2,16 @@
 
 input <- scan("Day_6_input.txt", sep = "", what = "character")
 input <- strsplit(input, split = "")[[1]]
+len <- 14
 
 flag <- "F"
 i <- 1
 
 while (flag == "F") {
-    word <- input[i:(i + 3)]
-    if (length(unique(word)) == 4) {
+    word <- input[i:(i + len - 1)]
+    if (length(unique(word)) == len) {
         flag <- "T"
-        print(i + 3)
+        print(i + len - 1)
     }
     i <- i + 1
 }
