95eeb75e7f84c3a5ddaa9438c687545681703c24
DavidMcMorris <david.mcmorris@huskers.unl.edu>
2022-12-17 13:35:05 -0800

    Finished Part 2

--- Day 10/Day10.R
+++ Day 10/Day10.R
@@ -2,7 +2,7 @@
 
 input <- read.table("Day_10_input.txt", sep = "", fill = TRUE, colClasses = c("character", "numeric"), row.names = NULL, col.names = c("V1", "V2"))
 
-tick <- 1
+tick <- 0
 value <- 1
 strength <- NULL
 
@@ -26,3 +26,36 @@ for (i in seq_len(dim(input)[1])) {
 }
 
 print(sum(strength))
+
+tick <- 0
+value <- 1
+pixels <- NULL
+
+drawer <- function(tick, value) {
+    if (is.element(tick %% 40, c(value - 1, value, value + 1))) {
+        draw <- "#"
+    } else {
+        draw <- "."
+    }
+    return(draw)
+}
+
+for (i in seq_len(dim(input)[1])) {
+    if (input[i, 1] == "noop") {
+        draw <- drawer(tick, value)
+        pixels <- c(pixels, draw)
+        tick <- tick + 1
+    } else {
+        draw <- drawer(tick, value)
+        pixels <- c(pixels, draw)
+        tick <- tick + 1
+        draw <- drawer(tick, value)
+        pixels <- c(pixels, draw)
+        tick <- tick + 1
+        value <- value + input[i, 2]
+    }
+}
+
+graphic <- data.frame(matrix(pixels, ncol = 40, byrow = TRUE))
+words <- which(x == "#", arr.ind = TRUE)
+plot(words[, 2], -words[, 1], ylim = c(-40, 30), pch = 15)
