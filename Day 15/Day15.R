dc4e199ed3a0ac24c3ee7818f7e5a0432b706f9e
DavidMcMorris <david.mcmorris@huskers.unl.edu>
2023-08-03 11:58:05 -0400

    Part 2 naive approach, will run forever

--- Day 15/Day15.R
+++ Day 15/Day15.R
@@ -2,6 +2,8 @@
 
 input <- readLines("Day_15_input.txt")
 lvl <- 2000000
+max <- 4000000
+
 
 input <- unlist(strsplit(input, split = ":"))
 input <- unlist(strsplit(input, split = ","))
@@ -10,6 +12,11 @@ input <- as.numeric(gsub("[^0-9-]", "", input))
 input <- matrix(input, ncol = 4, byrow = TRUE)
 
 input <- cbind(input, rowSums(abs(input[, 1:2] - input[, 3:4])))
+
+tc_dist <- function(a, b) {
+    distance <- sum(abs(a - b))
+    return(distance)
+}
 
 check_lvl <- function(sensor, level) {
     dist2lvl <- abs(sensor[2] - level)
@@ -27,3 +34,21 @@ seen_at_lvl <- length(unique(seen_at_lvl[which(!is.na(seen_at_lvl))]))
 beacons_at_lvl <- sum(unique(input[, 3:4])[, 2] == lvl)
 
 print(seen_at_lvl - beacons_at_lvl)
+counter <- 0
+total <- max * max
+
+for (i in 0:max) {
+    for (j in 0:max) {
+        vis <- FALSE
+        for (k in seq_len(nrow(input))){
+            if (vis == FALSE) {
+                sensor <- c(input[k, 1], input[k, 2])
+                beacon_dist <- input[k, 5]
+                vis <- tc_dist(sensor, c(i, j)) <= beacon_dist
+            }
+        }
+        if (vis == FALSE) {
+            print(c(i, j))
+        }
+    }
+}
